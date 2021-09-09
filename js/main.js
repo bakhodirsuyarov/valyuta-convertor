0493cdd07718252946ac3fa4f2bf01c138e253c8
bakhodirsuyarov <83678993+bakhodirsuyarov@users.noreply.github.com>
2021-09-09 08:15:15 +0500

    initial

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let elForm = document.querySelector('.js-form');
+let elInput = document.querySelector('.js-input');
+let elSelect = document.querySelector('.js-select');
+let elOutput = document.querySelector('.js-output');
+
+elForm.addEventListener('submit', function(evt) {
+  evt.preventDefault()
+
+  let resault = Number(elInput.value) * Number(elSelect.value);
+
+  elOutput.textContent = resault.toFixed(2);
+});
+
+
